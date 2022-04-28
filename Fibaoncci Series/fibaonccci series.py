3aabf02690b70a15ba433d4a5478c2eee9e992f1
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-04-28 15:54:33 +0530

    program to print fibaoncci series

---
+++ Fibaoncci Series/fibaonccci series.py
@@ -0,0 +1,16 @@
+# python program to print fibonacci series upto n terms
+
+terms = int(input("Enter number of terms:"))
+
+first = 0  # first number of the series
+second = 1 # second number of the series
+sum = 0 # sum of the series
+
+print("Fibonacci series:")
+for i in range(0, terms):  # for loop to print the series
+    print(sum, "+")
+    sum = first + second
+    first = second
+    second = sum
+
+print("=", sum)
