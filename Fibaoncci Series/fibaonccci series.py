d331d223f2ce90317fe4396b67e27c4eacc3b825
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-04-28 17:24:23 +0530

    modify code

--- Fibaoncci Series/fibaonccci series.py
+++ Fibaoncci Series/fibaonccci series.py
@@ -3,11 +3,11 @@
 terms = int(input("Enter number of terms:"))
 
 first = 0  # first number of the series
-second = 1 # second number of the series
-sum = 0 # sum of the series
+second = 1  # second number of the series
+sum = 0  # sum of the series
 
 print("Fibonacci series:")
-for i in range(0, terms):  # for loop to print the series
+for i in range(1, terms+1):  # for loop to print the series
     print(sum, "+")
     sum = first + second
     first = second
