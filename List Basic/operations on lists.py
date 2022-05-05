c11388a719e6999f231085d048c0baa7aa29c591
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-05 08:15:33 +0530

    python program to demonstrate operations on lists

---
+++ List Basic/operations on lists.py
@@ -0,0 +1,9 @@
+# python program to demonstrate the operations that can be performed on lists
+
+numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  # list of numbers
+numbers2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]  # list of numbers
+
+# printing the list
+print(numbers1 + numbers2)  # concatenation of two lists using '+' operator
+print(numbers1 * 3)  # multiplication of list using '*' operator
+print(numbers1[1:4])  # slicing of list using ':' operator
