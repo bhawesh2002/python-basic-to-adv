95f91f1f729bf02f1b3c40c663ae64791bfb783d
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-06 06:53:50 +0530

    add some more basic operations

--- List Basic/list_basic.py
+++ List Basic/list_basic.py
@@ -1,17 +1,31 @@
-terms = int (input("Enter the number of terms in the number list: "))   #prompt the user to enter the number of terms in list
+# prompt the user to enter the number of terms in list
+terms = int(input("Enter the number of terms in the number list: "))
 
-numbers = []    #declare the lsit of numbers
+numbers = []  # declare the lsit of numbers
 
-for i in range (0,terms):   #use of for loop to fill the number list
-    print("Enter the", i + 1 ,"number: ", end = "") #prompting the user to enter a number
-    num = int (input()) #get input number from the user
-    numbers.append(num) #use of append() function to add number entered by user into number list
+for i in range(0, terms):  # use of for loop to fill the number list
+    # prompting the user to enter a number
+    print("Enter the", i + 1, "number: ", end="")
+    num = int(input())  # get input number from the user
+    # use of append() function to add number entered by user into number list
+    numbers.append(num)
 
-print ("The Maximum number in the list is: ", max(numbers)) #use of max() function to get maximum number in the list
-print ("The Minimum number in the list is: ", min(numbers)) #use of min() function to get minimum number in the list
+# use of max() function to get maximum number in the list
+print("The Maximum number in the list is: ", max(numbers))
+# use of min() function to get minimum number in the list
+print("The Minimum number in the list is: ", min(numbers))
 
-Sum = sum(numbers)  #use of sum() function to calculate the sum of numbers in the number list
-#formula to calculate avg of numbers
-Avg = Sum/len(numbers) #use of len() function to calculate number of terms in the number list 
-print("The Sum of numbers in the list is: ", Sum)  #print the sum of numbers
-print("The Averge of numbers in the list is: ", Avg) #print the average of numbers
+# use of sum() function to calculate the sum of numbers in the number list
+Sum = sum(numbers)
+# formula to calculate avg of numbers
+# use of len() function to calculate number of terms in the number list
+Avg = Sum/len(numbers)
+print("The Sum of numbers in the list is: ", Sum)  # print the sum of numbers
+# print the average of numbers
+print("The Averge of numbers in the list is: ", Avg)
+print("The reverse of the list is: ")  # print the reverse of the list
+# use of slicing to print the reverse of the list
+print("Use of slicing: ", numbers[::-1])
+# use of reverse() function to print the reverse of the list
+revNumbers = numbers.reverse()
+print("Using  reverse() function", revNumbers)
