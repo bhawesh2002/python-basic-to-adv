519c1eee8b72e2dff48b4c22c0e4851936acd51c
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-07 11:05:43 +0530

    use of dictionary

---
+++ Dictonary/dictonary_intro.py
@@ -0,0 +1,7 @@
+#A python code to demonstrate the use of dictionary
+
+friut_price = {'apple': 2.50, 'banana': 3.00, 'orange': 3.50}
+
+print("Apple",friut_price['apple'])
+print("Banana",friut_price['banana'])
+print("Orange",friut_price['orange'])
