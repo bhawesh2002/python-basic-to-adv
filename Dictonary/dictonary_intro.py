a75e50c761ba5e28c03f72523ccc8884fefb3d9d
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-07 11:06:22 +0530

    comment the code

--- Dictonary/dictonary_intro.py
+++ Dictonary/dictonary_intro.py
@@ -1,7 +1,9 @@
-#A python code to demonstrate the use of dictionary
+# A python code to demonstrate the use of dictionary
 
+# create a dictionary storing the price of fruits
 friut_price = {'apple': 2.50, 'banana': 3.00, 'orange': 3.50}
+# dictionary is a collection of key-value pairs
 
-print("Apple",friut_price['apple'])
-print("Banana",friut_price['banana'])
-print("Orange",friut_price['orange'])
+print("Apple", friut_price['apple'])
+print("Banana", friut_price['banana'])
+print("Orange", friut_price['orange'])
