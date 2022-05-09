d9703390b742105ba07c090a2a96c21de575fb6b
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-09 12:55:09 +0530

    Update result.py

--- Result/result.py
+++ Result/result.py
@@ -1,32 +1,29 @@
-# python program to print result of student
+# python program to calculate result of a student
 
-print("Result System")
-
-name = input("Enter your name:")
-
-print("Enter your subjectwise marks")
-
-maths = float(input("Maths:"))
-english = float(input("English:"))
-science = float(input("Science:"))
-marathi = float(input("Marathi:"))
-social_studies = float(input("Social Studies:"))
-
-sum = maths + english + science + marathi + social_studies
-
-print("Total Marks = ", sum)
-
-percentage = sum/500*100
-
-print("Percentage = ", percentage , "%")
-
-if(percentage >= 90):
-    print("Conguratulations! You have secured A grade")
-elif(percentage >= 80):
-    print("Conguratulations! You have secured B grade")
-elif(percentage >= 70):
-    print("Conguratulations! You have secured C grade")
-elif(percentage >= 60):
-    print("Conguratulations! You have secured D grade")
+print("======GRADE CALCULATOR======")
+name = input("Enter Your Name: ")
+print("Enter Your Marks in each subject")
+subject_marks = ["English", "Marathi", "Maths", "Science", "Social Studies"]
+for i in range(0, len(subject_marks)):
+    print(subject_marks[i], ": ", end="")
+    marks = float(input())
+    subject_marks[i] = marks
+print("============================")
+total_marks = sum(subject_marks)
+percentage = total_marks * (100/(len(subject_marks)*100))
+print("TOTAL MARKS: ", total_marks)
+print("PERCENTAGE: ", percentage, "%")
+print("============================")
+if(percentage > 85):
+    print(name, " YOU ARE PASSED WITH DISTINCTION")
+elif(percentage > 75):
+    print(name, " YOU ARE PASSED WITH GRADE A")
+elif(percentage > 60):
+    print(name, " YOU ARE PASSED WITH GRADE B")
+elif(percentage > 50):
+    print(name, " YOU ARE PASSED WITH GRADE C")
+elif(percentage > 45):
+    print(name, " YOU ARE PASSED WITH GRADE D")
 else:
-    print("Fail")
+    print("YOU ARE FAILED")
+print("============================")
