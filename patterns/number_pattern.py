677067d99d2bff5294b3e1f74f7a24bd6408a848
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-09 12:30:01 +0530

    remove unnessary variable

--- patterns/number_pattern.py
+++ patterns/number_pattern.py
@@ -3,8 +3,7 @@
 num = int(input("Enter a number: "))
 print("0\n")
 for i in range(0, num):
-    a = i
     for j in range(0, i+1):
-        print(a + 1, end="")
-        a = a + 1
+        print(i + 1, end="")
+        i = i + 1
     print("\n")
