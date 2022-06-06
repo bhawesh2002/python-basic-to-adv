0a77c7d9e852bec4722168a6409ce1d380ca8d10
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-06 12:38:36 +0530

    comment the code

--- math/square and cube.py
+++ math/square and cube.py
@@ -1,25 +1,27 @@
-import math
+import math  #to use functions defined in math library
 
-num = int(input("Enter a number:"))
+num = int(input("Enter a number:"))  #get the input
 
-sqRoot = math.sqrt(num)
-sq = num*num
-cube = num*num*num
-fact = 1
-print("The Square root of ", num, " is ", sqRoot)
-print("The Cube of ", num, " is ", cube)
-for i in range(2, num):
-    if(num % i == 0):
-        print(num, " is not a prime number")
-        break
-    if(i == num - 1):
+sqRoot = math.sqrt(num) #use sqrt function defined in math library
+sq = num*num #store the square of number
+cube = num*num*num #store the cube of number
+fact = 1 #initialise factorial to 1
+print("The Square root of ", num, " is ", sqRoot) #print the square root
+print("The Cube of ", num, " is ", cube) #print the cube root
+for i in range(2, num): #for loop
+    if(num % i == 0): #check weather number is completely divisible 
+        print(num, " is not a prime number")#if divisible by i print is prime
+        break #break the loop if number is divisible by i
+    if(i == num - 1):  #condition for prime number
         print(num, "is a prime number")
 
-for i in range(1, num+1):
+for i in range(1, num+1): #for loop to compute factorial
     fact *= i
 
-print(num, "! = ", fact)
-print("Prime factors of ", num, " are")
+print(num, "! = ", fact) #print factorial
+print("Prime factors of ", num, " are")#print prime factorial
 for i in range(1, num + 1):
     if(num % i == 0):
         print(i, " ", end="")
+
+
