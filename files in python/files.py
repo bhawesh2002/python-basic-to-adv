a0d884b2661d794f228b11f2a181c874321ddf4d
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-07-04 12:14:51 +0530

    some changes

--- files in python/files.py
+++ files in python/files.py
@@ -5,3 +5,12 @@ for line in f:
     no_of_lines += 1
 
 print("The no of lines in foo.txt are: ",no_of_lines )
+f.close()
+file1 = open("info.txt", "w")
+L = ["This is Delhi \n", "This is Paris \n", "This is London \n"]
+file1.write("Hello \n")
+with open("steal.txt","w+") as steal:
+    for line in file1:
+        steal.write(line)
+        
+print("The information is stolen")
