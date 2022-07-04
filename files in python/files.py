38afa16e102a97d48b7b0f3af7594b8ed6b42639
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-07-04 11:56:13 +0530

    open the file in read mode

--- files in python/files.py
+++ files in python/files.py
@@ -1,3 +1,7 @@
-f = open("files in python/foo.txt","r+t",encoding='utf-8') #Open a file in python
+f = open("files in python/foo.txt","r") #Open a file in python
+no_of_lines = 0
 for i in f:
-    print(f.read())
+    print(f.readline(12))
+    no_of_lines += 1
+
+print("The no of lines in foo.txt are: ",no_of_lines )
