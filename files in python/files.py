1537a27a9896ee6c9a71a0dfd9721661292283ff
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-07-04 12:06:23 +0530

    count the no of lines in foo.txt

--- files in python/files.py
+++ files in python/files.py
@@ -1,7 +1,7 @@
 f = open("files in python/foo.txt","r") #Open a file in python
 no_of_lines = 0
-for i in f:
-    print(f.readline(12))
+for line in f:
+    print(line)
     no_of_lines += 1
 
 print("The no of lines in foo.txt are: ",no_of_lines )
