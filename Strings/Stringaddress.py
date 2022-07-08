b8d04b29158fab18a790c657446280e9456cfbae
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-07-08 12:57:01 +0530

    fix the code

--- Strings/Stringaddress.py
+++ Strings/Stringaddress.py
@@ -9,13 +9,8 @@ print("Str2 is : ", str2)
 
 print("ID of strl is : ", id(str2))
 
-stri += str2
+str1 += str2
 
-print("Strl after concatenation is: ", stri)
+print("Strl after concatenation is: ", str1)
 
-print("ID of stri is : ", id(stri))
-
-str3 += str1
-print("str3 = ", str3)
-
-print("ID of str3 is: ", id(str3))
+print("ID of stri is : ", id(str1))
