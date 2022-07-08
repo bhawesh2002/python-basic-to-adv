87882c1134c7e9a0bc28663beee0ea2bdec6908a
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-07-08 13:12:37 +0530

    iterate using whilw loop

--- Strings/slicing string.py
+++ Strings/slicing string.py
@@ -8,3 +8,9 @@ print(str[1:-3])
 print(str[::-1])
 print("Name" not in str)
 print("Jimmy" in str)
+for i in str:
+    print(i,end="")
+i = 0
+while (i< len(str)):
+    print(str[i],end="")
+    i+=1
